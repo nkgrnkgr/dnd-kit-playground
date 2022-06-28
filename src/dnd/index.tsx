1725b9aff113dd2fe760cb1df40c6cdd2483cc66
taiki-nishi <taiki-nishi@cybozu.co.jp>
2022-06-28 09:10:07 +0900

    add storybook

--- src/dnd/index.tsx
+++ src/dnd/index.tsx
@@ -1,18 +1,27 @@
-import { DndContext } from "@dnd-kit/core";
+import { DndContext, DragEndEvent } from "@dnd-kit/core";
+import { useState } from "react";
 import { Draggable } from "./Draggable";
 import { Droppable } from "./Droppable";
 
 const Component: React.FC = () => {
+  const [isDropped, setIsDropped] = useState(false);
+
+  const handleDragEnd = (e: DragEndEvent) => {
+    if (e.over && e.over.id === "droppable") {
+      setIsDropped(true);
+    }
+  };
+
   return (
-    <DndContext>
-      <Droppable>
-        <div>this is droppable</div>
-      </Droppable>
-      <Draggable>
-        <div>this is draggable</div>
-      </Draggable>
+    <DndContext onDragEnd={handleDragEnd}>
+      {!isDropped ? <DraggableComponent /> : null}
+      <Droppable>{isDropped ? <DraggableComponent /> : "Drop here"}</Droppable>
     </DndContext>
   );
+};
+
+const DraggableComponent: React.FC = () => {
+  return <Draggable>Drag me</Draggable>;
 };
 
 export const Dnd = Component;
